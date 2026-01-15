4f173be0524fc6f578812c279d89eb7908f5122c
Chang Jia Jun <135327869+jiajunchang2002g@users.noreply.github.com>
2026-01-15 16:19:08 +0800

    Echo

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -1,10 +1,18 @@
-public class Duke {
+import java.util.Scanner;
+
+public class Jarvis {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello, I am Jarvis.");
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        do {
+            System.out.print("You: ");
+            input = scanner.nextLine();
+            System.out.println("Jarvis: " + input);
+        } while (!input.equalsIgnoreCase("bye"));
+
+        System.out.println("Bye!");
+        scanner.close();
     }
 }
