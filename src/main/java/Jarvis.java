89d47785d642a87a876e2c6f97c7a50d42605dd7
Chang Jia Jun <135327869+jiajunchang2002g@users.noreply.github.com>
2026-01-15 16:40:09 +0800

    Add, List

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -1,18 +1,47 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Jarvis {
-    public static void main(String[] args) {
-        System.out.println("Hello, I am Jarvis.");
+
+    public void greet() {
+        System.out.println("Hello! I am Jarvis, your personal assistant.");
+    }
+
+    public void farewell() {
+        System.out.println("Goodbye! Have a great day!");
+    }
+
+    public void run() {
+        greet();
 
         Scanner scanner = new Scanner(System.in);
-        String input;
-        do {
-            System.out.print("You: ");
-            input = scanner.nextLine();
-            System.out.println("Jarvis: " + input);
-        } while (!input.equalsIgnoreCase("bye"));
+        ArrayList<Task> tasks = new ArrayList<>();
+        while(true) {
+            String input = scanner.nextLine();
+            switch(input) {
+                case "list":
+                    printTasks(tasks);
+                    break;
+                case "bye":
+                    farewell();
+                    scanner.close();
+                    return;
+                default:
+                    Task task = new Task(input);
+                    tasks.add(task);
+                    System.out.println("Task added.");
+            }
+        }
+    }
 
-        System.out.println("Bye!");
-        scanner.close();
+    public void printTasks(ArrayList<Task> tasks) {
+        System.out.println("Here are your tasks:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i).toString());
+        }
+    }
+    public static void main(String[] args) {
+        Jarvis jarvis = new Jarvis();
+        jarvis.run();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    private String task;
+    private boolean isDone;
+
+    public Task(String task) {
+        this.task = task;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return (isDone ? "[X] " : "[ ] ") + task;
+    }
+}
